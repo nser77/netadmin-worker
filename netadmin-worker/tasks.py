76c748962646e0fd697506b6779a54aba4c39e50
nser77 <104022475+nser77@users.noreply.github.com>
2022-12-21 22:08:47 +0100

    Create tasks.py

---
+++ netadmin-worker/tasks.py
@@ -0,0 +1,7 @@
+import time
+from celery import shared_task
+
+@shared_task(name="worker.tasks.broad_message")
+def broad_message(message, *args, **kwargs):
+    print(message["router"])
+    return True
