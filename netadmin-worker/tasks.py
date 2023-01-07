05d6d79366f9ed600747d3e017c5296ecc2e043c
nser77 <104022475+nser77@users.noreply.github.com>
2023-01-08 00:25:13 +0100

    Update tasks.py

--- netadmin-worker/tasks.py
+++ netadmin-worker/tasks.py
@@ -5,7 +5,7 @@ from maxmind import MaxMind
 @shared_task(name="worker.tasks.broad_message")
 def broad_message(message, *args, **kwargs):
     mm=MaxMind()
-    mm.download_key="IBxxeFAv8DZa6lUt"
+    mm.download_key="000000"
     if mm.getCsvDB():
         with mm.gl2_country_blocks_ipv4_db.open() as f:
             index_db=csv.reader(f)
