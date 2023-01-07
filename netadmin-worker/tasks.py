4425f798a7fdd182ebc8b1ec73fb7a1fe414ea43
nser77 <104022475+nser77@users.noreply.github.com>
2023-01-08 00:25:02 +0100

    Update tasks.py

--- netadmin-worker/tasks.py
+++ netadmin-worker/tasks.py
@@ -1,7 +1,17 @@
+import csv
 from celery import shared_task
 from maxmind import MaxMind
 
 @shared_task(name="worker.tasks.broad_message")
 def broad_message(message, *args, **kwargs):
-    print(message["router"])
+    mm=MaxMind()
+    mm.download_key="IBxxeFAv8DZa6lUt"
+    if mm.getCsvDB():
+        with mm.gl2_country_blocks_ipv4_db.open() as f:
+            index_db=csv.reader(f)
+            for al in message["address_list"]:
+                for r in index_db:
+                    print(r)
+        f.close()
+
     return True
